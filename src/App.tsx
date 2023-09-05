9bd38a490fa1d09a4faeb6e411971e307936f408
Andrew <andrew_ilyushchenko@mail.ru>
2023-09-05 20:41:13 +0300

    feat: add cart page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import CartPage from './pages/CartPage';
 
 
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigate replace to='home' />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/home' element={<HomePage />} />
+      <Route path='/cart' element={<CartPage />} />
     </Routes>
   );
 }
